cd9bf0aa1e5761b26b67659cc143d24b308c0981
54145a <133004095+54145a@users.noreply.github.com>
2026-06-24 12:25:26 +0800

    force osc52 clipboard (2nd attempt)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,13 +3,21 @@
 -- Add any additional options here
 vim.opt.expandtab = false
 
-vim.o.clipboard = "unnamedplus"
-vim.api.nvim_create_autocmd("TextYankPost", { -- https://github.com/LazyVim/LazyVim/discussions/1565
-  callback = function()
-    -- vim.highlight.on_yank()
-    local copy_to_unnamedplus = require("vim.ui.clipboard.osc52").copy("+")
-    copy_to_unnamedplus(vim.v.event.regcontents)
-    local copy_to_unnamed = require("vim.ui.clipboard.osc52").copy("*")
-    copy_to_unnamed(vim.v.event.regcontents)
-  end,
-})
+-- https://github.com/LazyVim/LazyVim/discussions/4602
+if vim.env.SSH_TTY then
+  vim.opt.clipboard:append("unnamedplus")
+  local function paste()
+    return vim.split(vim.fn.getreg(""), "\n")
+  end
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+end
