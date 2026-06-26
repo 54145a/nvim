34e46a43b4aaadb3ca651d87186584afc060adb7
54145a <133004095+54145a@users.noreply.github.com>
2026-06-26 11:11:40 +0800

    tmp

---
+++ lua/plugins/custom.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      on_colors = function(colors)
+        if vim.o.background == "dark" then
+          colors.bg = "NONE"
+        end
+      end,
+    },
+  },
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {},
+  },
+	{
+		"lambdalisue/suda.vim",
+		cmd = { "SudaRead", "SudaWrite" },
+	},
+	{
+			"github/copilot.vim"
+	}
+}
