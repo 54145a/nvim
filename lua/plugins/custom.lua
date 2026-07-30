ff860a606a787b626bbaeffc825c517448251a9a
54145a <133004095+54145a@users.noreply.github.com>
2026-07-30 21:02:40 +0800

    tmp

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,11 +1,7 @@
 return {
 	{
 		"folke/tokyonight.nvim",
-		opts = {
-			on_colors = function(colors)
-				colors.bg = "NONE"
-			end,
-		},
+		opts = {},
 	},
 	{
 		"f-person/auto-dark-mode.nvim",
